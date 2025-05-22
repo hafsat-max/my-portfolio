81b348a71f2d2f5bc10f3549323b5c15a0c822d8
Abiodun Busari <hafsahbusari69@gmail.com>
2025-05-22 21:51:19 +0200

    fix: uncomment github link

--- app/constants/social-links.ts
+++ app/constants/social-links.ts
@@ -1,12 +1,13 @@
 import { CiLinkedin } from "react-icons/ci";
+import { RiGithubLine } from "react-icons/ri";
 
 export const socialLinks = [
   {
     Icon: CiLinkedin,
     link: "https://www.linkedin.com/in/hafsah-busari/",
   },
-  // {
-  //   Icon: RiGithubLine,
-  //   link: "https://github.com/hafsat-max",
-  // },
+  {
+    Icon: RiGithubLine,
+    link: "https://github.com/hafsat-max",
+  },
 ];
